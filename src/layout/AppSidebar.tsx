d1316ee3bed889c46c8dbcd1d22e91a95e8f7949
qevanoraofficial <qevanoraofficial@gmail.com>
2026-08-13 03:28:11 +0700

    Add Blacklist sidebar menu

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -8,6 +8,19 @@ import FreelanceIcon from "../icons/freelance.svg";
 import HomeIcon from "../icons/home.svg";
 import TestimonialMenuIcon from "../icons/testimonial-menu.svg";
 
+const BlacklistIcon = () => (
+  <svg
+    aria-hidden="true"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2Zm0 2c1.85 0 3.55.63 4.9 1.69L5.69 16.9A7.902 7.902 0 0 1 4 12c0-4.41 3.59-8 8-8Zm0 16c-1.85 0-3.55-.63-4.9-1.69L18.31 7.1A7.902 7.902 0 0 1 20 12c0 4.41-3.59 8-8 8Z" />
+  </svg>
+);
+
 const navItems = [
   { icon: <HomeIcon />, name: "ʜᴏᴍᴇ", path: "/" },
   {
@@ -19,6 +32,11 @@ const navItems = [
     icon: <TestimonialMenuIcon />,
     name: "ᴛᴇꜱᴛɪᴍᴏɴɪ",
     path: "/testimonials",
+  },
+  {
+    icon: <BlacklistIcon />,
+    name: "ʙʟᴀᴄᴋʟɪꜱᴛ",
+    path: "/blacklist",
   },
 ];
 
